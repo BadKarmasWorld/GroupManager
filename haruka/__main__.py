abd148b71c2c69d4759f3edb77a542eed6cc7daa
shlok2121 <72356399+shlok2121@users.noreply.github.com>
2020-10-05 09:59:33 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -649,8 +649,4 @@ def process_update(self, update):
         except Exception:
             self.logger.exception('An uncaught error was raised while processing the update')
 
-
-if __name__ == '__main__':
-    LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
-    LOGGER.info("Successfully loaded")
     
